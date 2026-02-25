e4cc71a426b1bc7db702b75b00427f6d1a776fe3
Yash Kumawat <itsyashkumawat@gmail.com>
2026-02-25 21:01:20 +0530

    Initiate the Learning

---
+++ File-Handling/file.js
@@ -0,0 +1,7 @@
+// console.log("Here we Start File Handling")
+
+const fs = require("fs");
+
+
+// Synchronous Call
+fs.writeFileSync('./test.txt','Hey YASH 1 2 3');
---
+++ helloWorld/app.js
@@ -0,0 +1,9 @@
+// const math = require("./math,js");
+
+const {add,sub}= require("./math,js")
+
+// console.log("Hello, Here we start Node JS");
+
+console.log("Math File VAlue is : ", add(56,89));
+console.log("Math File VAlue is : ", sub(56,89));
+// console.log(add(54,56));
